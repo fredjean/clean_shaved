75238d918dddb723831748e20918713fe0fd5cf9
Frederic Jean <fred@fredjean.net>
2012-09-21 11:48:42 -0600

    Trimming down the dummy app for development.

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -13,11 +13,10 @@ Dummy::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+
+  CleanShaved.config.compile = false
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
